57a64daf591e6c0497740f0dbf5f9dd0b0c50955
krishna <krishna263430003219@gmail.com>
2025-09-17 11:28:34 +0530

    update ternary oprator inside ternary oprator

--- StudentScore.java
+++ StudentScore.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 public class StudentScore {
     public static void main(String[] args) {
-        int score = 85;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter the Marks::");
+        int score = sc.nextInt();
         String grade = (score >= 90) ? "A Grade" :
                        (score >= 80) ? "B Grade" :
                        (score >= 70) ? "C Grade" :
                        (score >= 60) ? "D Grade" : "F Grade";
         System.out.println("The grade is: " + grade);
+        // using ternary oprater inside ternary operator
+        String possibelGrade = (score > 80) ? "High" : (score > 60 ? "Medium" : "Low") ;
+
+        System.out.println("Now the possible grade type is::" +possibelGrade);
+
+
     }
     
 }
